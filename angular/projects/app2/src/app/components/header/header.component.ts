2f2a3b9ddd03489def474f3ad61e94b50b7a591f
thiago-anjos <thiago.w3c@gmail.com>
2022-02-17 22:57:10 -0300

    debounceTime together with subject and observable and switchmap

--- angular/projects/app2/src/app/components/header/header.component.ts
+++ angular/projects/app2/src/app/components/header/header.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { debounceTime, Observable, Subject } from 'rxjs';
 import { OfertasService } from 'src/app/services/ofertas.service';
 import { OfertasModel } from 'src/app/shared/models/ofertas-model';
+import { switchMap } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  public ofertas: Observable<OfertasModel[]> | undefined;
+  public ofertas!: Observable<OfertasModel[]>;
+  private subjectPesquisa: Subject<string> = new Subject();
 
   constructor(private ofertasService: OfertasService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.ofertas = this.subjectPesquisa.pipe(
+      debounceTime(1000),
+      switchMap((termo: string) => {
+        return this.ofertasService.pesquisaOfertas(termo);
+      })
+    );
+
+    this.ofertas.subscribe((oferta: OfertasModel[]) =>
+      console.log('retorno 1 segundo depois', oferta)
+    );
+  }
 
   public pesquisa(termoDaBusca: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
-    this.ofertas.subscribe((item) => console.log(item));
+    //para unir o termo da pesquisa e disparar depois de um tempo, parecido com o debounce
+    console.log('evento keyup acontecendo', termoDaBusca);
+    this.subjectPesquisa.next(termoDaBusca);
   }
 }
--- angular/projects/app2/src/app/services/ofertas.service.ts
+++ angular/projects/app2/src/app/services/ofertas.service.ts
@@ -4,40 +4,41 @@ import { OfertasModel } from '../shared/models/ofertas-model';
 import { Observable } from 'rxjs';
 import { baseUrl } from '../shared/app.api';
 import { OfertasComoUsar } from '../shared/models/ofertas-como-usar';
+import { retry } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class OfertasService {
   public ofertas: Array<OfertasModel> = [];
 
-  constructor(public Http: HttpClient) {}
+  constructor(public http: HttpClient) {}
 
   getOfertas(): Observable<Array<OfertasModel>> {
     //efetuar uma requisição http e retornar uma promessa contendo um array de ofertas
-    return this.Http.get<OfertasModel[]>(`${baseUrl}/ofertas`);
+    return this.http.get<OfertasModel[]>(`${baseUrl}/ofertas`);
   }
 
   getOfertaPorCategoria(categoria: string): Observable<Array<OfertasModel>> {
-    return this.Http.get<OfertasModel[]>(
+    return this.http.get<OfertasModel[]>(
       `${baseUrl}/ofertas?categoria=${categoria}`
     );
   }
 
   getOfertaPorId(id: number): Observable<OfertasModel[]> {
-    return this.Http.get<OfertasModel[]>(`${baseUrl}/ofertas?id=${id}`);
+    return this.http.get<OfertasModel[]>(`${baseUrl}/ofertas?id=${id}`);
   }
 
   getComoUsarOfertaPorId(id: number): Observable<OfertasComoUsar[]> {
-    return this.Http.get<OfertasComoUsar[]>(`${baseUrl}/como-usar?id=${id}`);
+    return this.http.get<OfertasComoUsar[]>(`${baseUrl}/como-usar?id=${id}`);
   }
 
   getOndeFica(id: number): Observable<OfertasComoUsar[]> {
-    return this.Http.get<OfertasComoUsar[]>(`${baseUrl}/onde-fica?id=${id}`);
+    return this.http.get<OfertasComoUsar[]>(`${baseUrl}/onde-fica?id=${id}`);
   }
 
   pesquisaOfertas(termo: string): Observable<OfertasModel[]> {
-    return this.Http.get<OfertasModel[]>(
-      `${baseUrl}/ofertas?descricao_oferta_like=${termo}`
-    );
+    return this.http
+      .get<OfertasModel[]>(`${baseUrl}/ofertas?descricao_oferta_like=${termo}`)
+      .pipe(retry(2));
   }
 }
