e067177844624d15e253f075e109182818717e40
thiago-anjos <thiago.w3c@gmail.com>
2022-02-20 22:20:41 -0300

    chore: app use LOCALE_ID pt-br

--- angular/projects/app2/src/app/app.module.ts
+++ angular/projects/app2/src/app/app.module.ts
@@ -1,6 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +28,7 @@ import { OfertasModule } from './views/ofertas/ofertas.module';
     OndeFicaComponent,
   ],
   imports: [BrowserModule, HttpClientModule, OfertasModule, AppRoutingModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
