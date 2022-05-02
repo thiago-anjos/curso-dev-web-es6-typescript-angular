5e958e80bde77ea261ccc865dc6b2c0cd9a38cc8
thiago-anjos <your_email@example.com>
2022-05-01 23:52:46 -0300

    fix ordem-compra error after change formModule to ReactFormsModule

--- angular/projects/app2/src/app/components/ordem-compra/ordem-compra.component.ts
+++ angular/projects/app2/src/app/components/ordem-compra/ordem-compra.component.ts
@@ -1,32 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from 'src/app/services/ordem-compra.service';
-import { Pedido } from 'src/app/shared/models/pedido-compra';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.scss'],
+  providers: [OrdemCompraService],
 })
 export class OrdemCompraComponent implements OnInit {
-  constructor(public ordemCompraService: OrdemCompraService) {}
+  constructor(private ordemCompraService: OrdemCompraService) {}
 
-  @ViewChild('formulario') public formulario!: NgForm;
+  ngOnInit() {}
 
-  ngOnInit(): void {}
-
-  idPedidoCompra: number = -1;
-
-  enviarDados(): void {
-    let pedido: Pedido = {
-      complemento: this.formulario.value.complemento,
-      endereco: this.formulario.value.endereco,
-      formaPagamento: this.formulario.value.formaPagamento,
-      numero: this.formulario.value.numero,
-    };
-
-    this.ordemCompraService.efetivarCompra(pedido).subscribe((item: Pedido) => {
-      this.idPedidoCompra = item.id;
-    });
-  }
+  public confirmarCompra(): void {}
 }
