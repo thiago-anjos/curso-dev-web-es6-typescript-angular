ecefb51c417870409b77a4533a45a57715f4bfeb
thiago-anjos <your_email@example.com>
2022-06-23 23:09:58 -0300

    animação banner e painel

--- angular/projects/app3/src/app/components/acesso/acesso.component.ts
+++ angular/projects/app3/src/app/components/acesso/acesso.component.ts
@@ -1,15 +1,52 @@
+import {
+  style,
+  trigger,
+  state,
+  animate,
+  transition,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.scss']
+  styleUrls: ['./acesso.component.scss'],
+  animations: [
+    trigger('animacao-banner', [
+      state(
+        'criado',
+        style({
+          opacity: 1,
+        })
+      ),
+      //void state
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-100px, 0)' }),
+        animate('500ms 0s ease-in-out'),
+      ]),
+    ]),
+    trigger('animacao-painel', [
+      state(
+        'criado',
+        style({
+          opacity: 1,
+        })
+      ),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(50px,0)',
+        }),
+        animate('500ms 0s ease-in-out'),
+      ]),
+    ]),
+  ],
 })
 export class AcessoComponent implements OnInit {
+  public estadoBanner: string = 'criado';
+  public estadoPainel: string = 'criado';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
